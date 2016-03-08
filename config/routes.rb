5223eb7c14a91db60f5e611bfc9b4e5b67b4c477
Freddy Mounir <fmounir@pluseg.com>
2016-03-08 19:54:57 +0200

    git

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
  
    resource :emailtemplates     
    resource :home_page
-
+   
+   get 'emailtemplates/index' => 'emailtemplates#index'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
