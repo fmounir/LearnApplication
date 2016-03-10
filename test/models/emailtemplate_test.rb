f3c2a4a00d3d96f303f195cf0c85251b705a31bf
Freddy Mounir <fmounir@pluseg.com>
2016-03-10 13:51:48 +0200

    String

--- test/models/emailtemplate_test.rb
+++ test/models/emailtemplate_test.rb
@@ -1,17 +1,19 @@
 require 'test_helper'
 
 class EmailtemplateTest < ActiveSupport::TestCase
+  setup do
+    @emailTemplate = Emailtemplate.new
+  end
   
   test "shouldnt save empty key" do
-    emailTemplate = Emailtemplate.new
-    emailTemplate.key = nil
-    emailTemplate.body = "string"
-    emailTemplate.subject = "string"
-    assert_not emailTemplate.save 
+  
+    @emailTemplate.key = nil
+    @emailTemplate.body = "string"
+    @emailTemplate.subject = "string"
+    assert_not @emailTemplate.save 
   end
   
    test "shouldnt save empty subject" do
-    emailTemplate = Emailtemplate.new
     emailTemplate.key = "string"
     emailTemplate.body = "string"
     emailTemplate.subject = nil
@@ -20,7 +22,6 @@ class EmailtemplateTest < ActiveSupport::TestCase
   
   
    test "shouldnt save empty body" do
-    emailTemplate = Emailtemplate.new
     emailTemplate.key = "string"
     emailTemplate.body = nil
     emailTemplate.subject = "string"
